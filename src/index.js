359cfc9f872bea768db6cbb51725da5a2c274d34
Viraj Perera <vperera@care.local>
2024-03-28 10:17:23 +0100

    add react-snap to build html files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -6,32 +7,19 @@ import SmoothScroll from './components/SmoothScroll';
 import Scrollbar from './components/ScrollBar';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
-import { hydrate, render } from "react-dom";
-import { createRoot } from 'react-dom/client';
-
 
 
 AOS.init();
 
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement); // createRoot(container!) if you use TypeScript
-
-if (rootElement.hasChildNodes()) {
-  hydrate(  
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Scrollbar/>
     <SmoothScroll/>
     <App />
-  </React.StrictMode>, rootElement);
-} else {
-  root.render(  
-  <React.StrictMode>
-    <Scrollbar/>
-    <SmoothScroll/>
-    <App />
-  </React.StrictMode>);
-}
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
